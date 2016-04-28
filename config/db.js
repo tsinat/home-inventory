fb999421f1338dbe0e567474a9b7e369162c34ed
Tsinat Zeree <cheru87@gmail.com>
2016-04-27 20:53:15 -0700

    config change

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ var db = mysql.createConnection(process.env.JAWSDB_URL || {
   database : 'testdb'
 });
 
-connection.connect();
+db.connect();
 
 db.query(`create table if not exists rooms(id int primary key auto_increment, name text)`);
 db.query(`create table if not exists items(id int primary key auto_increment, name text, value int, category text, rooms int)`);
