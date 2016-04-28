246d892493ce3d0e63ba2a5ccdc2b880d73f6169
Tsinat Zeree <cheru87@gmail.com>
2016-04-27 20:48:53 -0700

    db config change

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var db = mysql.createConnection(process.env.JAWSDB_URL || {
   host     : 'localhost',
   user     : 'root',
   password : 'cheru',
